70bb86a968ea17d7fc29284e14ad75719016ab36
Jacobwwiz <jacobwwiz@gmail.com>
2025-08-05 14:36:07 -0500

    added course poll services

---
+++ src/services/CoursePollServices.js
@@ -0,0 +1,27 @@
+import apiClient from "./services";
+
+export default {
+  // Get all courses linked to a specific poll
+  getCoursesByPollId(pollId) {
+    return apiClient.get("coursePolls/" + pollId);
+  },
+
+  // Create a new course-poll link
+  createCoursePollLink(pollId, courseId) {
+    const link = { pollId, courseId };
+    return apiClient.post("coursePolls/", link);
+  },
+
+  // Update a course-poll link for a specific poll
+  updateCoursePollLink(pollId, oldCourseId, newCourseId) {
+    return apiClient.put("coursePolls/" + pollId, {
+      oldCourseId,
+      newCourseId,
+    });
+  },
+
+  // Delete all course-poll links for a specific poll
+  deleteAllLinksForPoll(pollId) {
+    return apiClient.delete("coursePolls/" + pollId);
+  },
+};
