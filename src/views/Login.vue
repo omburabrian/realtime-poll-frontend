9a2411450169cb6b1aa5cd8c5b61f3885bf6fc62
Glen Davis <glen.davis@oc.edu>
2025-08-07 10:53:27 -0500

    Reordered create account input fields - username and email

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -41,15 +41,14 @@ function navigateToPollsHistory() {
 }
 
 async function createAccount() {
-  await UserServices.addUser(newUser.value)
-    .then(() => {
-      showSnackbar("Account created successfully!", "green");
-      closeCreateAccount();
-    })
-    .catch((error) => {
-      console.log(error);
-      showErrorSnackbar(error);
-    });
+  try {
+    await UserServices.addUser(newUser.value);
+    showSnackbar("Account created successfully");
+    closeCreateAccount();
+  } catch (error) {
+    //  console.log(error);
+    showErrorSnackbar(error, "Error creating account");
+  }
 }
 
 async function login() {
@@ -93,25 +92,14 @@ function closeCreateAccount() {
         <v-form @submit.prevent="login">
           <v-card-title class="headline mb-2">Login </v-card-title>
           <v-card-text>
-            <v-text-field
-              v-model="loginCredentials.email"
-              label="Email or Username"
-              required
-            ></v-text-field>
+            <v-text-field v-model="loginCredentials.email" label="Email or Username" required></v-text-field>
 
-            <v-text-field
-              v-model="loginCredentials.password"
-              :type="showPassword ? 'text' : 'password'"
+            <v-text-field v-model="loginCredentials.password" :type="showPassword ? 'text' : 'password'"
               :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append-inner="showPassword = !showPassword"
-              label="Password"
-              required
-            ></v-text-field>
+              @click:append-inner="showPassword = !showPassword" label="Password" required></v-text-field>
           </v-card-text>
           <v-card-actions>
-            <v-btn variant="flat" color="secondary" @click="openCreateAccount()"
-              >Create Account</v-btn
-            >
+            <v-btn variant="flat" color="secondary" @click="openCreateAccount()">Create Account</v-btn>
             <v-spacer></v-spacer>
 
             <v-btn variant="flat" color="primary" type="submit">Login</v-btn>
@@ -121,12 +109,7 @@ function closeCreateAccount() {
 
       <v-card class="rounded-lg elevation-5 my-8">
         <v-card-title class="text-center headline">
-          <v-btn
-            class="ml-2"
-            variant="flat"
-            color="secondary"
-            @click="navigateToPollsHistory()"
-          >
+          <v-btn class="ml-2" variant="flat" color="secondary" @click="navigateToPollsHistory()">
             View Polls History
           </v-btn>
         </v-card-title>
@@ -137,50 +120,22 @@ function closeCreateAccount() {
           <v-form @submit.prevent="createAccount">
             <v-card-title class="headline mb-2">Create Account </v-card-title>
             <v-card-text>
-              <v-text-field
-                v-model="newUser.firstName"
-                label="First Name"
-                required
-              ></v-text-field>
+              <v-text-field v-model="newUser.firstName" label="First Name" required></v-text-field>
 
-              <v-text-field
-                v-model="newUser.lastName"
-                label="Last Name"
-                required
-              ></v-text-field>
+              <v-text-field v-model="newUser.lastName" label="Last Name" required></v-text-field>
 
-              <v-text-field
-                v-model="newUser.username"
-                label="Username"
-                required
-              ></v-text-field>
+              <v-text-field v-model="newUser.email" label="Email" required></v-text-field>
 
-              <v-text-field
-                v-model="newUser.email"
-                label="Email"
-                required
-              ></v-text-field>
+              <v-text-field v-model="newUser.username" label="Username" required></v-text-field>
 
-              <v-text-field
-                v-model="newUser.password"
-                :type="showPassword ? 'text' : 'password'"
+              <v-text-field v-model="newUser.password" :type="showPassword ? 'text' : 'password'"
                 :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                @click:append-inner="showPassword = !showPassword"
-                label="Password"
-                required
-              ></v-text-field>
+                @click:append-inner="showPassword = !showPassword" label="Password" required></v-text-field>
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn
-                variant="flat"
-                color="secondary"
-                @click="closeCreateAccount()"
-                >Close</v-btn
-              >
-              <v-btn variant="flat" color="primary" type="submit"
-                >Create Account</v-btn
-              >
+              <v-btn variant="flat" color="secondary" @click="closeCreateAccount()">Close</v-btn>
+              <v-btn variant="flat" color="primary" type="submit">Create Account</v-btn>
             </v-card-actions>
           </v-form>
         </v-card>
